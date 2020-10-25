a03e084a76140010b5a7396b57e9ac09e2b75106
ionpetro <ionpetro@gmail.com>
2020-10-25 21:52:09 +0200

    add search bar on favorite movies component

--- src/app/movies/favorite/favorite.component.ts
+++ src/app/movies/favorite/favorite.component.ts
@@ -11,12 +11,14 @@ import { UserService } from 'src/app/services/user.service';
 export class FavoriteComponent implements OnInit {
   favoriteMoviesObservable: Observable<Movie[]>;
   favoriteMovies: Movie[];
+  filteredMovies: Movie[];
   constructor(private userService: UserService) {}
 
   ngOnInit(): void {
     this.favoriteMoviesObservable = this.userService.getFavoriteMovies();
     this.favoriteMoviesObservable.subscribe((movies) => {
       this.favoriteMovies = movies;
+      this.filteredMovies = movies;
     });
   }
 
@@ -28,6 +30,14 @@ export class FavoriteComponent implements OnInit {
         this.favoriteMovies = this.favoriteMovies.filter(
           (x) => movie.id !== x.favoriteId
         );
+        this.filteredMovies = this.favoriteMovies;
       });
+  }
+
+  filter(query: string) {
+    query = query.toLowerCase().trim();
+    this.filteredMovies = this.favoriteMovies.filter((movie) =>
+      movie.title.includes(query)
+    );
   }
 }

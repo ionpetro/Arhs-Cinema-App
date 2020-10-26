9d1d299ce3c24ce43efe6bbc87a00523de62e550
ionpetro <ionpetro@gmail.com>
2020-10-26 13:13:23 +0200

    minor additions

--- src/app/movies/movie-card/movie-card.component.ts
+++ src/app/movies/movie-card/movie-card.component.ts
@@ -1,4 +1,11 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnChanges,
+  OnInit,
+  Output,
+} from '@angular/core';
 import { Movie } from 'src/app/models/movie';
 import { MoviesService } from 'src/app/services/movies.service';
 import { UserService } from 'src/app/services/user.service';
@@ -8,7 +15,7 @@ import { UserService } from 'src/app/services/user.service';
   templateUrl: './movie-card.component.html',
   styleUrls: ['./movie-card.component.scss'],
 })
-export class MovieCardComponent implements OnInit {
+export class MovieCardComponent implements OnInit, OnChanges {
   @Input('movie') movie: Movie;
   @Input('favorite') favorite: boolean;
   @Output('delete') deleteEvent: EventEmitter<void> = new EventEmitter<void>();
@@ -26,6 +33,10 @@ export class MovieCardComponent implements OnInit {
   constructor(private userService: UserService) {}
 
   ngOnInit(): void {
+    this.selected = Boolean(this.movie.favoriteId);
+  }
+
+  ngOnChanges(): void {
     this.selected = Boolean(this.movie.favoriteId);
   }
 

f65df92a6b0e0cff975b7926c7335d20c60dbe89
ionpetro <ionpetro@gmail.com>
2020-10-26 12:58:40 +0200

    fix back button on preview component

--- src/app/movies/movie-preview/movie-preview.component.ts
+++ src/app/movies/movie-preview/movie-preview.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { Movie } from 'src/app/models/movie';
 import { MoviesService } from 'src/app/services/movies.service';
@@ -14,11 +15,16 @@ export class MoviePreviewComponent implements OnInit {
 
   constructor(
     private moviesService: MoviesService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private _location: Location
   ) {}
 
   ngOnInit(): void {
     this.id = this.route.snapshot.params['id'];
     this.moviesService.getMovieById(this.id).subscribe((x) => (this.movie = x));
+  }
+
+  back() {
+    this._location.back();
   }
 }

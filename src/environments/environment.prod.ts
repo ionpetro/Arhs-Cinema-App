6231ef0b202b98edfc2e38f08384ce3edf4e13b8
ionpetro <ionpetro@gmail.com>
2020-10-29 15:19:01 +0200

    deploy app to firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  config: {
+    omdbApi: {
+      url: 'https://www.omdbapi.com/',
+      apiKey: '5f938b38',
+    },
+  },
+  apiUrl: 'https://us-central1-arhs-cinema.cloudfunctions.net/app',
 };
